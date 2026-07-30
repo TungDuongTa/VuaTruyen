7f42e536af67b45887034f5eeb16b364c1ead45e
Tung Duong Ta <duongtatung@gmail.com>
2026-07-30 17:43:00 +1000

    remove unnecessary preconenct

--- app/layout.tsx
+++ app/layout.tsx
@@ -109,10 +109,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="vi" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://cdn.vuatruyen.cc" />
-        <link rel="dns-prefetch" href="https://cdn.vuatruyen.cc" />
-      </head>
       <body className={`${inter.variable} font-sans antialiased`}>
         <ThemeProvider
           attribute="class"
